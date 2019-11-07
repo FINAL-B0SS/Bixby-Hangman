45495b254c511f3966715798b3e6883560e5437e
Maljean <maljean@student.42.us.org>
2019-11-06 21:55:25 -0800

    Added more games

--- code/Games.js
+++ code/Games.js
@@ -1,12 +1,127 @@
 module.exports = [
   {
     answer: "Scarface",
-    hints: ['Takes place in miami', 'Main character is from cuba'],
+    hints: ['Takes place in Miami', 'Main character is from cuba', 'Involves drugs'],
     category: ['Movies']
   },
   {
     answer: "The\nLion\nKing",
     hints: ['Disney film', 'Has Live action and animated version'],
     category: ['Movies']
-  }
+  },
+  {
+    answer: "Nissan",
+    hints: ['Built and Sold British Cars for Over Seven Years', 'Built Rocket Engines', ' Global Headquarters is Located in Yokohama, Japan'],
+    category: ['Car manufacturers']
+  },
+  {
+    answer: "Honda",
+    hints: ['Originally manufactured motorized bicycles', 'A major exporter of soybeans', 'Has its own airport'],
+    category: ['Car manufacturers']
+  },
+  {
+    answer: "Toyota",
+    hints: [],
+    category: ['Car manufacturers']
+  },
+  {
+    answer: "Subaru",
+    hints: ['FujiHeavy Industries is the parent company', 'Name and logo comes from the Pleiades constellation', 'Loves animals'],
+    category: ['Car manufacturers']
+  },
+  {
+    answer: "Toyota",
+    hints: ['Not in Forza Horizon 4', 'Technically still a mom-and-pop style family company', 'Made their first car in 1937'],
+    category: ['Car manufacturers']
+  },
+  {
+    answer: "Ferrari",
+    hints: ['Badge was a war veteran\'s coat of arms', 'Cars are not the only income stream for the brand', 'Bought out by Ford'],
+    category: ['Car manufacturers']
+  },
+  {
+    answer: "Lamborghini",
+    hints: [],
+    category: ['Car manufacturers']
+  },
+  {
+    answer: "Aston Martin",
+    hints: ['Founded in 1913', 'Second car was produced in 1919', 'Augustus Bertelli Designed the Cars Between 1926 and 1937'],
+    category: ['Car manufacturers']
+  },
+  {
+    answer: "Tesla",
+    hints: ['The second oldest publicly listed American Automaker', 'Third most shorted on American stock exchanges', 'Based in Silicon Valley'],
+    category: ['Car manufacturers']
+  },
+  {
+    answer: "Maserati",
+    hints: ['Started Out with Race Cars', 'Founded in Bologna', 'Logo Is Based on a Fountain'],
+    category: ['Car manufacturers']
+  },
+  {
+    answer: "Volkswagen",
+    hints: [],
+    category: ['Car manufacturers']
+  },
+  {
+    answer: "Dodge",
+    hints: ['Started out making bicycles', 'Helped create the first mass-produced American car', 'Cars were used in the first “mechanical” cavalry in US history'],
+    category: ['Car manufacturers']
+  },
+  {
+    answer: "Hot dog",
+    hints: [],
+    category: ['Foods']
+  },
+  {
+    answer: "Apple pie",
+    hints: [],
+    category: ['Foods']
+  },
+  {
+    answer: "Macaroni and cheese",
+    hints: [],
+    category: ['Foods']
+  },
+  {
+    answer: "Tater Tots",
+    hints: [],
+    category: ['Foods']
+  },
+  {
+    answer: "Cheeseburger",
+    hints: [],
+    category: ['Foods']
+  },
+  {
+    answer: "Pancakes",
+    hints: [],
+    category: ['Foods']
+  },
+  {
+    answer: "Potato Chips",
+    hints: [],
+    category: ['Foods']
+  },
+  {
+    answer: "Cornbread",
+    hints: [],
+    category: ['Foods']
+  },
+  {
+    answer: "Nachos",
+    hints: [],
+    category: ['Foods']
+  },
+  {
+    answer: "Burrito",
+    hints: [],
+    category: ['Foods']
+  },
+  {
+    answer: "Lasagna",
+    hints: [],
+    category: ['Foods']
+  },
 ]
